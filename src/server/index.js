1869b3760834b663c33e06184073a1655c719a7c
Davi-Quirino <davi.quirino@ab-inbev.com>
2024-09-11 19:58:56 -0300

    change index vercel

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+//const http = require("http");
+//const { Server } = require("socket.io");
 
 // Configurando o servidor express
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+//const server = http.createServer(app);
+//const io = new Server(server);
 
 // Rota padrão GET
 app.get("/", (req, res) => {
@@ -21,53 +21,53 @@ app.get("/", (req, res) => {
 //   },
 // });
 
-let players = [];
-let isRevealed = false; // Estado global que controla se as cartas estão reveladas
+// let players = [];
+// let isRevealed = false; // Estado global que controla se as cartas estão reveladas
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
 
-  // Envia a lista completa de jogadores ao novo cliente
-  socket.emit("currentPlayers", players);
+//   // Envia a lista completa de jogadores ao novo cliente
+//   socket.emit("currentPlayers", players);
 
-  // Recebe a informação quando um jogador entra
-  socket.on("joinGame", (player) => {
-    player.socketId = socket.id; // Salva o socketId do jogador
-    players.push(player);
-    io.emit("playerJoined", player);
-  });
+//   // Recebe a informação quando um jogador entra
+//   socket.on("joinGame", (player) => {
+//     player.socketId = socket.id; // Salva o socketId do jogador
+//     players.push(player);
+//     io.emit("playerJoined", player);
+//   });
 
-  // Recebe a seleção de uma carta por um jogador
-  socket.on("selectCard", (updatedPlayer) => {
-    players = players.map((player) =>
-      player.id === updatedPlayer.id ? updatedPlayer : player
-    );
-    io.emit("cardSelected", updatedPlayer);
-  });
+//   // Recebe a seleção de uma carta por um jogador
+//   socket.on("selectCard", (updatedPlayer) => {
+//     players = players.map((player) =>
+//       player.id === updatedPlayer.id ? updatedPlayer : player
+//     );
+//     io.emit("cardSelected", updatedPlayer);
+//   });
 
-  // Recebe o comando para revelar as cartas
-  socket.on("revealCards", () => {
-    isRevealed = true; // Altera o estado global de revelação
-    io.emit("revealCards", true); // Notifica todos os clientes para revelar as cartas
-  });
+//   // Recebe o comando para revelar as cartas
+//   socket.on("revealCards", () => {
+//     isRevealed = true; // Altera o estado global de revelação
+//     io.emit("revealCards", true); // Notifica todos os clientes para revelar as cartas
+//   });
 
-  // Evento de reset dos jogadores (resetPlayers)
-  socket.on("resetPlayers", (resetPlayers) => {
-    players = resetPlayers; // Atualiza a lista de jogadores no servidor
-    io.emit("currentPlayers", players); // Envia a lista atualizada para todos os clientes
-  });
+//   // Evento de reset dos jogadores (resetPlayers)
+//   socket.on("resetPlayers", (resetPlayers) => {
+//     players = resetPlayers; // Atualiza a lista de jogadores no servidor
+//     io.emit("currentPlayers", players); // Envia a lista atualizada para todos os clientes
+//   });
 
-  // Lida com a desconexão do jogador e o remove da lista
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-    players = players.filter((player) => player.socketId !== socket.id); // Remove o jogador da lista
-    io.emit("playerDisconnected", socket.id); // Notifica todos os clientes sobre a desconexão
-  });
+//   // Lida com a desconexão do jogador e o remove da lista
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected");
+//     players = players.filter((player) => player.socketId !== socket.id); // Remove o jogador da lista
+//     io.emit("playerDisconnected", socket.id); // Notifica todos os clientes sobre a desconexão
+//   });
 
-  // Evento de novo jogo - reinicia o jogo para todos os jogadores
-  socket.on("newGame", () => {
-    players = []; // Reseta os jogadores no servidor
-    isRevealed = false; // Reseta o estado de revelação
-    io.emit("newGame"); // Notifica todos os clientes para reiniciar o jogo
-  });
-});
+//   // Evento de novo jogo - reinicia o jogo para todos os jogadores
+//   socket.on("newGame", () => {
+//     players = []; // Reseta os jogadores no servidor
+//     isRevealed = false; // Reseta o estado de revelação
+//     io.emit("newGame"); // Notifica todos os clientes para reiniciar o jogo
+//   });
+// });
