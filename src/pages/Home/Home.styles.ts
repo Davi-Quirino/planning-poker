c662630fadb9ceea01a1e448af2ad27faee2fe43
Davi-Quirino <davi.quirino@ab-inbev.com>
2024-09-23 18:31:08 -0300

    socket

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -2,16 +2,13 @@ import styled from "styled-components";
 
 export const TableContainer = styled.div`
   position: relative;
-  width: 100vw; // A largura será 100% da largura da viewport
-  height: 50vh; // A altura será 50% da altura da viewport
-  overflow: hidden; // Evita que o conteúdo exceda o tamanho da mesa
+  width: 100%;
+  height: 200px; // Ajuste a altura conforme necessário
 `;
 
 export const TableImage = styled.img`
-  width: 100%; // Mantém a imagem ajustada ao container responsivamente
-  height: auto; // Mantém a proporção original da imagem
-  max-height: 100%;
-  object-fit: cover; // Garante que a imagem sempre cubra o container
+  width: 100%;
+  height: 800px;
 `;
 
 export const PlayerPosition = styled.div<{
@@ -20,14 +17,6 @@ export const PlayerPosition = styled.div<{
   position: absolute;
   top: ${(props) => props.position?.top || "0"};
   left: ${(props) => props.position?.left || "0"};
-  transform: translate(-50%, -50%); // Centraliza os jogadores no ponto definido
-  width: 5vw; // Controla o tamanho do jogador de forma relativa à viewport
-  height: 5vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  z-index: 5;
 `;
 
 export const RevealButton = styled.button`
