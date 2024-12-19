b7836b8068daaf330167ce4342ad9cfefc7d7473
Davi-Quirino <davi.quirino@ab-inbev.com>
2024-12-19 15:50:28 -0300

    removing unused

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ModalContainer, ModalContent } from "./Modal.styles";
+import { ModalContainer } from "./Modal.styles";
 
 interface ModalProps {
   isOpen: boolean;
@@ -10,14 +10,7 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
-  return (
-    <ModalContainer>
-      {/* <ModalContent> */}
-      {children}
-      {/* <button onClick={onClose}>Close</button> */}
-      {/* </ModalContent> */}
-    </ModalContainer>
-  );
+  return <ModalContainer>{children}</ModalContainer>;
 };
 
 export default Modal;
